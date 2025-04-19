d7ef04204a03e4616e59b6268c43322bfc872252
vedantgarg2005 <vedantravigarg64@gmail.com>
2025-04-19 22:42:21 +0530

    Add frontend/src/App.js 1745082742694

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Footer from './components/Footer';
+import AdminDashboard from './pages/AdminDashboard';
+import DeliveryDashboard from './pages/DeliveryDashboard';
 
 function App() {
   return (
@@ -12,13 +14,17 @@ function App() {
           <h1 className="text-4xl font-bold text-blue-600">Quick-Commerce</h1>
           <div>
             <Link to="/" className="mr-4 text-blue-600 hover:underline">Home</Link>
-            <Link to="/login" className="text-blue-600 hover:underline">Login</Link>
+            <Link to="/login" className="mr-4 text-blue-600 hover:underline">Login</Link>
+            <Link to="/admin" className="mr-4 text-blue-600 hover:underline">Admin</Link>
+            <Link to="/delivery" className="text-blue-600 hover:underline">Delivery</Link>
           </div>
         </nav>
         <div className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/delivery" element={<DeliveryDashboard />} />
           </Routes>
         </div>
         <Footer />
